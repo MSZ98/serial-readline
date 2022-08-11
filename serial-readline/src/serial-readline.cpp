86226a8d2fff24ece35ec1bc9e92fdfef8ae9d9a
MSZ98 <51297472+MSZ98@users.noreply.github.com>
2022-08-11 20:03:26 +0200

    Update serial-readline.cpp

--- serial-readline/src/serial-readline.cpp
+++ serial-readline/src/serial-readline.cpp
@@ -1,7 +1,8 @@
-/**
+/*
  * serial-readline.cpp - Library for buffered serial line reading
  * Created by MSZ, March 3, 2022.
- * Released into the public domain.
+ * Released into the public domain under MIT Licence.
+ * Copyright (c) 2022 Zawisza (MSZ98)
 */
 
 #include "serial-readline.h"
